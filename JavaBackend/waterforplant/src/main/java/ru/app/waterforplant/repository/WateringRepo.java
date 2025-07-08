57fd7ac4e737464d70e08ec24f40c63bd54274d0
maerdlina <58111252+maerdlina@users.noreply.github.com>
2025-07-08 12:54:37 +0300

    Delete JavaBackend/waterforplant directory

--- JavaBackend/waterforplant/src/main/java/ru/app/waterforplant/repository/WateringRepo.java
+++
@@ -1,8 +0,0 @@
-package ru.app.waterforplant.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.app.waterforplant.model.Watering;
-
-public interface WateringRepo extends JpaRepository<Watering, Long> {
-    Watering findWateringById(Long id);
-}

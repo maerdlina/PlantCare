b2f39cde2c3b74266d5a39ab49f5490d4b857587
maerdlina <58111252+maerdlina@users.noreply.github.com>
2025-07-08 10:59:25 +0300

    cors add

--- Backend/waterforplant/src/main/java/ru/app/waterforplant/controller/PlantController.java
+++ Backend/waterforplant/src/main/java/ru/app/waterforplant/controller/PlantController.java
@@ -10,6 +10,7 @@ import java.util.Date;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = {"http://127.0.0.1:5500", "http://localhost:5500"})
 @RequestMapping("/plants")
 public class PlantController {
     private final PlantService plantService;
--- Backend/waterforplant/src/main/java/ru/app/waterforplant/controller/WateringController.java
+++ Backend/waterforplant/src/main/java/ru/app/waterforplant/controller/WateringController.java
@@ -7,6 +7,7 @@ import ru.app.waterforplant.service.WateringService;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = {"http://127.0.0.1:5500", "http://localhost:5500"})
 @RequestMapping("/watering")
 public class WateringController {
     private final WateringService wateringService;
